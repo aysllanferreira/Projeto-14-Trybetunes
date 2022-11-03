c3a4a2f64b981b63f8cd42f8ee6212f96a4da989
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-11-03 16:18:34 -0300

    Requisito 05 - feito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Header from '../components/Header';
 
 function Search() {
+  const inputRef = useRef();
+  const buttonRef = useRef();
+
+  const handleSearch = () => {
+    const { value } = inputRef.current;
+    if (value.length > 1) {
+      buttonRef.current.disabled = false;
+    } else {
+      buttonRef.current.disabled = true;
+    }
+  };
+
   return (
     <div data-testid="page-search">
       <Header />
+      <input
+        onChange={ handleSearch }
+        ref={ inputRef }
+        data-testid="search-artist-input"
+        type="text"
+      />
+      <button
+        ref={ buttonRef }
+        disabled
+        type="button"
+        data-testid="search-artist-button"
+      >
+        Pesquisar
+
+      </button>
     </div>
   );
 }
