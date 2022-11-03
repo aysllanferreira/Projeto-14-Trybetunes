88acb5be4003058ee33e6d010f00dddbb1e6b0c2
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-11-03 16:08:02 -0300

    Requisito 04 - feito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -16,8 +17,14 @@ function Header() {
 
   return (
     <header data-testid="header-component">
-      <h1>ola</h1>
       {isLoading ? <p data-testid="header-user-name">{user.name}</p> : <Loading />}
+      {' '}
+      <br />
+      <nav>
+        <Link data-testid="link-to-search" to="/search">Pesquisa</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favoritos</Link>
+        <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
+      </nav>
     </header>
   );
 }
