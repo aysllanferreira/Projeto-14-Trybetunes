7f4826ca451bb47c614a21d8daf4341e006c794d
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-11-03 19:26:12 -0300

    Requisito 08 - feito

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
 
 function MusicCard({
   musicName,
 }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const addFavorite = async () => {
+    setIsLoading(true);
+    await addSong(musicName);
+    setIsLoading(false);
+  };
   return (
     <div>
       <p>{musicName.trackName}</p>
@@ -15,6 +22,16 @@ function MusicCard({
         <code>audio</code>
         .
       </audio>
+      <label htmlFor="favMusic">
+        Favorita
+        <input
+          type="checkbox"
+          data-testid={ `checkbox-music-${musicName.trackId}` }
+          id="favMusic"
+          onClick={ addFavorite }
+        />
+      </label>
+      {isLoading && <p>Carregando...</p>}
     </div>
   );
 }
@@ -23,6 +40,7 @@ MusicCard.propTypes = {
   musicName: PropTypes.shape({
     trackName: PropTypes.string.isRequired,
     previewUrl: PropTypes.string.isRequired,
+    trackId: PropTypes.number.isRequired,
   }).isRequired,
 };
 
