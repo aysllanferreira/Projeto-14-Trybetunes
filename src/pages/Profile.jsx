5e559a9db7075cd6bd0e11d57fd8c7e063402297
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-11-03 21:21:55 -0300

    Requisito 13 - feito

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 function Profile() {
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const history = useHistory();
+
+  const fetchUser = async () => {
+    setLoading(true);
+    const response = await getUser();
+    setUser(response);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div data-testid="page-profile">
       <Header />
+      {loading ? <Loading /> : (
+        <div>
+          <img
+            data-testid="profile-image"
+            src={ user.image }
+            alt={ user.name }
+          />
+          <p data-testid="profile-name">{ user.name }</p>
+          <p data-testid="profile-email">{ user.email }</p>
+          <p>{user.description}</p>
+          <button
+            type="button"
+            onClick={ () => history.push('/profile/edit') }
+          >
+            Editar perfil
+          </button>
+        </div>
+      )}
     </div>
   );
 }
